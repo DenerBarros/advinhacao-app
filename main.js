0eff30588faf06126c6c95631d4ea94f18ced6e4
DenerBarros <denerbarros.eng@gmail.com>
2022-09-06 15:58:27 -0300

    Criado a lógica e adicionado no script

---
+++ main.js
@@ -0,0 +1,15 @@
+const randonNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
+function handleClick(event) {
+    event.preventDefault();
+    const inputNumber = document.querySelector('#inputNumber')
+
+    if(Number(inputNumber.value) == randonNumber) {
+        document.querySelector('.screen1').classList.add('hide');
+        document.querySelector('.screen2').classList.remove('hide');
+        document.querySelector('.screen2').innerText = `acertou em ${xAttempts} tentativas`;
+    }
+    xAttempts++;
+    console.log(xAttempts);
+}
