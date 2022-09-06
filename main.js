a16ebc112bddd089a0244e3fd4227b68b8d116fc
DenerBarros <denerbarros.eng@gmail.com>
2022-09-06 16:26:23 -0300

    Terminado os padrões de event do app

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+
 const randonNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
 
-function handleClick(event) {
+function handleTryClick(event) {
     event.preventDefault();
     const inputNumber = document.querySelector('#inputNumber')
 
     if(Number(inputNumber.value) == randonNumber) {
-        document.querySelector('.screen1').classList.add('hide');
-        document.querySelector('.screen2').classList.remove('hide');
-        document.querySelector('.screen2').innerText = `acertou em ${xAttempts} tentativas`;
+        screen1.classList.add('hide');
+        screen2.classList.remove('hide');
+        document.querySelector('.screen2 h2').innerText = `acertou em ${xAttempts} tentativas`;
     }
+    inputNumber.value = "";
     xAttempts++;
     console.log(xAttempts);
 }
+
+//Eventos
+
+const btnTry = document.querySelector('#btnTry');
+const btnReset = document.querySelector('#btnReset');
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function() {
+    screen1.classList.remove('hide');
+    screen2.classList.add('hide');
+    xAttempts = 1;
+})
